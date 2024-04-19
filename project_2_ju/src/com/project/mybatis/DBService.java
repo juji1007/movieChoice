4ef2671d8914fc69bb5af9049f210e17b3e68da2
minji-git <kimzz8168@gmail.com>
2024-04-19 18:38:26 +0900

    24.04.19 6:20PM

--- project_2_ju/src/com/project/mybatis/DBService.java
+++ project_2_ju/src/com/project/mybatis/DBService.java
@@ -1,8 +1,6 @@
 package com.project.mybatis;
 
 import java.io.IOException;
-import java.io.Reader;
-
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
