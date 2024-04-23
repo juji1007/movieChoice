f58049d01adb3c77cbec74e5db6a94296fe4d979
ITWILL <ITWILL@DESKTOP-6A2MMH4>
2024-04-23 11:37:38 +0900

    0423_fix

--- project_2_ju/src/com/mystudy/model/dao/postDAO.java
+++ project_2_ju/src/com/mystudy/model/dao/postDAO.java
@@ -76,17 +76,3 @@ public class postDAO {
 		}
 }
 
-public class postDAO {
-	//전체 자유게시판 글 목록 조회
-	public static List<postVO> postList() {
-		System.out.println("postList실행");
-		try (SqlSession ss = DBService.getFactory().openSession()) {
-			return ss.selectList("post.all");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-}
-
