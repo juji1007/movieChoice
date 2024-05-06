f4c59a149fa42d0854362152ebd800dd328d3cb3
user <user@juji1007>
2024-05-06 18:50:00 +0900

    0506

--- project_2_ju/src/com/project/ajax/controller/AjaxLoginController.java
+++ project_2_ju/src/com/project/ajax/controller/AjaxLoginController.java
@@ -22,6 +22,7 @@ import com.project.dao.AdminDAO;
 public class AjaxLoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
